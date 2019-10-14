7dc0634f18e6fb67a83b9020739873f7a767d9f1
Matas Rastenis <matas2k@gmail.com>
2019-10-15 00:28:38 +0200

    combined compose

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# get git, docker, pull docker compose selfcontained or not
+apt-get update 
+apt-get install git docker.io docker-compose -y
+
 # cloning/updating vue-storefront and vue-storefront-api
 for SOURCE in "vue-storefront" "vue-storefront-api"
 do
@@ -14,3 +18,13 @@ do
   fi
 
 done
+
+# setting up restoriations
+echo "yarn restore" >> vue-storefront-api/docker/vue-storefront-api/vue-storefront-api.sh 
+echo "yarn migrate" >> vue-storefront-api/docker/vue-storefront-api/vue-storefront-api.sh 
+
+# default configs
+cp ./vue-storefront/config/{default,local}.json
+
+# starting
+docker-compose up
